d90905132764c0fd49c78fe7f84ce39a3ac9dd99
Jianwen Ji <jijianwen@gmail.com>
2020-08-09 20:02:45 +0800

    Add n-ary-tree-level-order-traversal
    Signed-off-by: Jianwen Ji <jijianwen@gmail.com>

---
+++ Week_02/n-ary-tree-level-order-traversal.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        if root is None: return []
+
+        res = []
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                queue.extend(node.children)
+            res.append(level)
+        
+        return res
