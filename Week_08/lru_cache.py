3a1ee4742ee903ff6ca183b655c3521b61677e6f
Jianwen Ji <jijianwen@gmail.com>
2020-09-20 22:26:58 +0800

    update week 08 readme.md and add solution for lru-cache
    Signed-off-by: Jianwen Ji <jijianwen@gmail.com>

---
+++ Week_08/lru_cache.py
@@ -0,0 +1,78 @@
+class DLinkNode:
+    def __init__(self, key=0, val=0):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.size = 0
+        self.head = DLinkNode()
+        self.tail = DLinkNode()
+        self.cache = {}
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self.cache:
+            node = self.cache[key]
+            self.move_to_head(node)
+            return node.val
+        else:
+            return -1
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key not in self.cache:
+            node = DLinkNode(key, value)
+            self.cache[key] = node
+            self.add_to_head(node)
+            self.size += 1
+            if self.size > self.capacity:
+                node = self.remove_tail()
+                self.cache.pop(node.key)
+                self.size -= 1
+        else:
+            node = self.cache[key]
+            node.val = value
+            self.move_to_head(node)
+
+    def add_to_head(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def remove_node(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def move_to_head(self, node):
+        self.remove_node(node)
+        self.add_to_head(node)
+
+    def remove_tail(self):
+        node = self.tail.prev
+        self.remove_node(node)
+        return node
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
